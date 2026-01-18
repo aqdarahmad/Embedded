ef605e48f8a0bad30d01e30212ee850f40c3f0a0
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-01-18 18:55:03 +0200

    example 9-39

---
+++ ex9-39.c
@@ -0,0 +1,7 @@
+//Write a programm to toggle only the portB 4 bit continoulsy every 50 ms 
+//Using timer0 , 16 bit mode , the 1:4 prescaler
+
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
